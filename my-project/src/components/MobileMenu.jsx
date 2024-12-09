3ae808968971c3c99e9ed6fd2f6c5006805dcef5
Suraj Raikar <122537639+Surajaraikar@users.noreply.github.com>
2024-12-09 15:39:20 +0530

    Update MobileMenu.jsx

--- my-project/src/components/MobileMenu.jsx
+++ my-project/src/components/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import t2c from "../images/T2c.png";
+import t2c from "../images/T2C.png";
 import { useNavigate } from "react-router-dom";
 
 const MobileMenu = ({ onClose }) => {
