0f5b39c06e67b26a9c705c9ab713f1d82f197baf
Suraj Raikar <122537639+Surajaraikar@users.noreply.github.com>
2024-12-09 15:37:16 +0530

    Update Hero.jsx

--- my-project/src/Hero.jsx
+++ my-project/src/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Back from "./images/Background.svg";
-import t2c from "./images/T2c.png";
+import t2c from "./images/T2C.png";
 import MobileMenu from "./components/MobileMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
